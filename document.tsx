ec2440a8bac80fb3dba704dd7adcdffb4bbc71b2
Adwin120 <adam.winnik120@gmail.com>
2024-02-26 15:28:02 +0100

    fill in text data

--- document.tsx
+++ document.tsx
@@ -34,22 +34,48 @@ export const pdfContent = (
                     style={{ width: 200, borderRadius: 5 }}
                     src="images/me.jpg"
                 />
+                <Text>Kamieniec Wrocławski, 55-002, Dolnośląskie Poland</Text>
+                <Text>789158411</Text>
+                <Text>adam.winnik120@gmail.com</Text>
             </View>
-            <Section title="About me"></Section>
-            <Section title="Experience"></Section>
-            <Section title="Education"></Section>
+            <Section title="About me">
+            Solution-focused Fullstack Web Developer with broad knowledge of best software development practices and technologies. Able to quickly learn new skills and adapt company practices. Aiming to always maximize product maintainability to facilitate efficient team collaboration.
+            </Section>
+            <Section title="Work History">
+                <Text>Web Developer Intern</Text>
+                <Text>Grid Dynamics, Wrocław, Dolnośląskie, Poland</Text>
+                <Text>05-2022 - 03-2023</Text>
+                <Text>Worked in a SCRUM Agile development team</Text>
+                <Text>Developed user interfaces with React and Angular</Text>
+                <Text>Developed testing code for web-based applications</Text>
+                <Text>Developed user-friendly web forms with validation and error handling</Text>
+            </Section>
+            <Section title="Education">
+                <Text>Master of Science: Applicacation of Modern Information Technologies</Text>
+                <Text>Wrocław University of Science and Technology - Wrocław, Dolnośląskie, Poland</Text>
+                <Text>Bachelor of Science: Applied Computer Science</Text>
+                <Text>Wrocław University of Science and Technology - Wrocław, Dolnośląskie, Poland</Text>
+            </Section>
             <Section title="Skills">
-                <Text>Technologies I'm confidently able to work independently on</Text>
-                <Text>Technologies I'm able to work on under senior supervision</Text>
+                <Text>Web Development - HTML5, CSS3, ES6+, TS, React.JS, Angular, Node.JS</Text>
+                <Text>Javascript Tooling - Webpack, Jest, Eslint</Text>
+                <Text>Project Organization - Scrum Agile, Jira</Text>
+                <Text>Reactive Programming with ReactiveX</Text>
+                <Text>System Administration - Linux, Bash</Text>
+                <Text>Object Oriented Programming - Java, Python, C#</Text>
+                <Text>Functional Programming - Elixir, Scala, Haskell</Text>
+                <Text>Databases - MySQL, Firestore, Database Design, Data Warehousing</Text>
+                <Text>Cloud Providers - Google Cloud Platform (GCP)</Text>
             </Section>
             <Section title="Languages">
+                {/* <Text>• German - A1</Text> */}
                 <Text>• English - C1</Text>
                 <Text>• Polish - native</Text>
             </Section>
-            <Section title="Contact"></Section>
-            <View style={styles.chipSet}>
-                <Chip iconSrc="icons/haskell.png" title="React" />
-            </View>
+            {/* <Section title="Contact"></Section> */}
+            {/* <View style={styles.chipSet}>
+                <Chip iconSrc="icons/react.png" title="React" />
+            </View> */}
             <View>
                 <Text style={{ fontSize: 12 }}>
                     I hereby give consent for my personal data to be processed
