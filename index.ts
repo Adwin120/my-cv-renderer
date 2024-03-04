13bfda5e791d5b38e6f9b444f09995b5bc56a580
Adwin120 <adam.winnik120@gmail.com>
2024-03-04 10:09:46 +0100

    ensure dist file exists

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import ReactPDF, { Font } from "@react-pdf/renderer";
 import { pdfContent } from "./document";
+import fs from "fs"
 
 const FILE_NAME = "adamWinnikCV";
+const DIST_PATH = `${__dirname}/dist`;
 
-ReactPDF.render(pdfContent, `${__dirname}/dist/${FILE_NAME}.pdf`);
+fs.existsSync(DIST_PATH) || fs.mkdirSync(DIST_PATH)
+ReactPDF.render(pdfContent, `${DIST_PATH}/${FILE_NAME}.pdf`);
